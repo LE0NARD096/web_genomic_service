114d055541933fc35f9e29da94689d6603a14724
Leonardo <leonardo-ulderico.masci@universite-paris-saclay.fr>
2024-01-20 19:09:58 +0100

    deleting loader and media + modified gitignore

--- webannotation/loader/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class FastaFile(models.Model):
-    file = models.FileField(upload_to='fasta_files/')
-
-    def __str__(self):
-        return self.file.name
--- webannotation/loader/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import fasta_file_view
-
-urlpatterns = [
-    path('fasta/', fasta_file_view, name='fasta_file_view'),
-]
