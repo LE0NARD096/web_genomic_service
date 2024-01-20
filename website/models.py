2d307fa72fa5468e8f4513c333a4b647e6a42f11
Gaya El Haber <gaya.el-haber@universite-paris-saclay.fr>
2024-01-20 15:53:37 +0000

    Update models.py

--- website/models.py
+++ website/models.py
@@ -20,7 +20,7 @@ class User(AbstractUser):
         return self.first_name + " " + self.last_name
 
 class Genome(models.Model):
-    sequence = models.ForeignKey('Sequence', on_delete=models.CASCADE, related_name='genomes')
+    sequence = models.TextField()
     species = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     type = models.CharField(max_length=255)
@@ -30,3 +30,23 @@ class Genome(models.Model):
    
     def delete_everything(self):
         Genome.objects.all().delete()
+
+class Annotation(models.Model):
+    text = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='annotations')
+    sequence = models.OneToOneField('Sequence', on_delete=models.SET_NULL, null=True, blank=True)
+    isValidated = models.BooleanField(default=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Annotation by {self.user} - {self.text}"
+
+class Sequence(models.Model):
+    isUnique = models.BooleanField()
+    genome = models.ForeignKey(Genome, on_delete=models.CASCADE, related_name='sequences')
+    cdsFile = models.FileField(upload_to='cds_files/')
+    pepFile = models.FileField(upload_to='pep_files/')
+    
+    def __str__(self):
+        return f"Sequence for {self.genome}"
+    
