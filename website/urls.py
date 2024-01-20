d6508a5bba1ac339c7d9f7aaa3674c5312c3937f
Gaya El Haber <gaya.el-haber@universite-paris-saclay.fr>
2024-01-20 16:00:42 +0000

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name = "home"),
+    path('', views.home, name='home'),
     path('search/', views.search_results, name='search_results'),
-    path('upload/', views.upload, name='upload_file')
+    path('upload/', views.upload, name='upload_file'),
+    path('login/', views.login_view, name='login'),  
+    path('register/', views.register_view, name='register'),
+    # ... other url patterns ...
 ]
